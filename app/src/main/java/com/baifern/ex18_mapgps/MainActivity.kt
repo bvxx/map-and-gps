b8b8a5d0ea5697fb10d082f1f02b5b9b869f0763
bbxx <b_prueksanusak@hotmail.com>
2018-11-20 15:38:13 +0700

    Initial commit

---
+++ app/src/main/java/com/baifern/ex18_mapgps/MainActivity.kt
@@ -0,0 +1,94 @@
+package com.baifern.ex18_mapgps
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.Settings
+import android.support.v4.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private var locationManager: LocationManager? = null
+    private var locationListener: LocationListener? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationListener = object : LocationListener {
+            override fun onLocationChanged(location: Location?) {
+                latitudeText.setText(""+location!!.latitude)
+                longtitudeText.setText(""+location!!.longitude)
+            }
+
+            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+            }
+
+            override fun onProviderEnabled(provider: String?) {
+
+            }
+
+            override fun onProviderDisabled(provider: String?) {
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        }
+        requestLocation()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode) {
+            10 -> requestLocation()
+            else -> {}
+        }
+    }
+
+    private fun requestLocation() {
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET),10)
+            }
+            return
+        }
+        gpsBtn.setOnClickListener {
+            locationManager!!.requestLocationUpdates("gps",5000,0F,locationListener)
+        }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        markBtn.setOnClickListener {
+            var lat = latitudeText.text.toString().toDouble()
+            var long = longtitudeText.text.toString().toDouble()
+
+            val marker = LatLng(lat,long)
+            mMap.addMarker(MarkerOptions().position(marker).title(marker.toString()))
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+        }
+
+    }
+}
